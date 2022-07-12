de67ef0e03e1623cfe7e6f7f316426462cb9258f
Alex <alex1031alex@yandex.ru>
2022-07-12 19:47:37 +0300

    Added 3 components

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,12 +2,14 @@ import "./Main.css";
 import React from "react";
 import {Greeting} from "../Greeting/Greeting";
 import {About} from "../About/About";
+import {Works} from "../Works/Works";
 
 export const Main = () => {
   return (
     <main>
       <Greeting/>
       <About />
+      <Works />
     </main>
   );
 };
---
+++ src/Components/Works/Works.jsx
@@ -0,0 +1,12 @@
+import "./Works.css";
+import React from "react";
+
+export const Works = () => {
+  return (
+    <section className="works main__works">
+      <div className="container">
+        <h2 className="works__title">Works</h2>
+      </div>
+    </section>
+  );
+};
