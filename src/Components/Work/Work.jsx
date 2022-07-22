ea1f8239e505b055c64d2e0e9392f4b4c81846ba
Alex <alex1031alex@yandex.ru>
2022-07-22 16:12:41 +0300

    Added wrapping link to github with title prop to Work component

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -1,14 +1,16 @@
 import "./Work.css";
 import React from "react";
+import {Link} from "react-router-dom";
 
 export const Work = ({work}) => {
-
   return (
     <div className="work">
+      <a href={work.link} title="go to repository">
       <h3 className="work__title">{work.name}</h3>
       <p className="work__image-container">
         {work.image && <img src={work.image} />}
       </p>
+      </a>
       <div className="work__icon-container">
         {work.langs.map((it, index) => {
           if (index > 2) {
