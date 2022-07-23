5bb0458a8803079acefbfbcab8b58916fbcbc249
Alex <alex1031alex@yandex.ru>
2022-07-23 19:05:15 +0300

    Added loader

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import "./Loader.css";
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <div className="dot-spinner">
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+      <div className="dot-spinner__dot"/>
+    </div>
+  );
+};
--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -2,12 +2,18 @@ import "./Works.css";
 import React, {useEffect, useState} from "react";
 import {getWorks} from "../../api";
 import {Work} from "../Work/Work";
+import {Loader} from "../Loader/Loader";
 
 export const Works = () => {
   const [works, setWorks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     (async () => {
+      setIsLoading(true);
       const worksRawData = await getWorks();
+
+      setIsLoading(false);
       setWorks(worksRawData);
     })();
   },[]);
@@ -17,6 +23,7 @@ export const Works = () => {
       <div className="container">
         <h2 className="works__title">Works</h2>
         <div className="works__container">
+          {isLoading && <Loader size={60} speed={0.9} color={"#67568c"}/>}
           {works.map((work)=> {
             return (
               <div className="works__item" key={work.id}>
