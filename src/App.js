eaf3ff60d258862cde55b512beef16efe08dc07f
Alex <alex1031alex@yandex.ru>
2022-07-24 22:04:48 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import {Main} from "./Components/Main/Main";
+import {Works} from "./Components/Works/Works";
+import {Routes, Route} from "react-router-dom";
 import {EmptyPage} from "./Components/EmptyPage/EmptyPage";
+import {About} from "./Components/About/About";
+import {Contacts} from "./Components/Contacts/Contacts";
 
 function App() {
   return (
-    <EmptyPage>
-      <Main/>
-    </EmptyPage>
+    <Routes>
+      <Route path="/" element={<EmptyPage><Main/></EmptyPage>}/>
+      <Route path="/works/" element={<EmptyPage><Works /></EmptyPage>}/>
+      <Route path="/about/" element={<EmptyPage><About/></EmptyPage>} />
+      <Route path="/contacts" element={<EmptyPage><Contacts/></EmptyPage>} />
+    </Routes>
   );
 }
 
